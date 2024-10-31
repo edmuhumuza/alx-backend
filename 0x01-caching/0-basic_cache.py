27c9ca564e81e2b3070246b7fe83bcee144fce2a
edmuhumuza <muhumuzaedgar19@gmail.com>
2024-10-31 12:33:30 +0300

    Add .gitignore file

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """0-basic_cache module
 """
-
+from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """ BasicCache defines:
       - how to cache your data
       - how to retrieve cached data
     """
+    def __init__(self):
+        """ Initialize the cache_data dictionary """
+        super().__init__()
+
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key or item is None:
+        if key is None or item is None:
             return
-        else:
-            self.cache_data[key] = item
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item from the cache
         """
         if key is None:
             return
-        if key not in self.cache_data.keys:
+        if key not in self.cache_data.keys():
             return
         else:
             return self.cache_data[key]
